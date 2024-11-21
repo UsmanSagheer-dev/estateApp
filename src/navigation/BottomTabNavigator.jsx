85c9b6f29ab8888f96b282c768df35f9d5af73a0
Muhammad Usman <apnausmansagheer@gmail.com>
2024-11-22 00:19:07 +0500

    feat: apply svgs

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -3,12 +3,20 @@ import HomeScreen from '../screens/homescreen/HomeScreen';
 import Notification from '../screens/notification/Notification';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import SettingScreen from '../screens/setting/SettingScreen';
+import { Search } from '../assets/svg';
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen    options={{
+            title: 'My profile',
+            tabBarIcon: ({size,focused,color}) => {
+              return (
+                <Search/>
+              );
+            },
+          }} name="Home" component={HomeScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
       <Tab.Screen name="Notification" component={Notification} />
       <Tab.Screen name="Setting" component={SettingScreen} />
