1b72e9f8a19aee1fbbf3a42ef09795447b63450b
Muhammad Usman <apnausmansagheer@gmail.com>
2024-11-22 11:15:20 +0500

    completed bottom tab bar with icons and styles

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,3 +1,5 @@
-import Search from "../svg/Heart.svg"
-
-export {Search}
+import Profile from './Profile.svg'
+import Homeicon from './Homeicon.svg';
+import SearchIcon from './searchsvg.svg'
+import HeartIcon from './Heart.svg'
+export {HeartIcon, Homeicon,SearchIcon,Profile};
--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,26 +1,59 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homescreen/HomeScreen';
 import Notification from '../screens/notification/Notification';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import SettingScreen from '../screens/setting/SettingScreen';
-import { Search } from '../assets/svg';
+import {Homeicon, HeartIcon, SearchIcon, Profile} from '../assets/svg';
+
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen    options={{
-            title: 'My profile',
-            tabBarIcon: ({size,focused,color}) => {
-              return (
-                <Search/>
-              );
-            },
-          }} name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Notification" component={Notification} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#3498db',
+        tabBarInactiveTintColor: '#fff',
+        
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({size, focused, color}) => (
+            <Homeicon width={size} height={size} fill={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({size, focused, color}) => (
+            <SearchIcon width={size} height={size} fill={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({size, focused, color}) => (
+            <HeartIcon width={25} height={size} fill={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: ({size, focused, color}) => (
+            <Profile width={size} height={size} fill={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
-export default MyTabs 
+
+export default MyTabs;
